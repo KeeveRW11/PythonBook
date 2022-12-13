58fcaa153dfef5558eb15a829cf37e3eab664abd
KeeveRW11 <keevewhyte+github@gmail.com>
2022-12-12 22:14:37 -0500

    comma separated values

---
+++ 7. File IO/students.py
@@ -0,0 +1,32 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         #ADDING ONE VARIABLE PUTS EVERY THING IN ONE 
+#         row = line.rstrip().split(",")
+#         print(f"{row[0]} is in {row[1]}")
+
+# with open("students.csv") as file:
+#     for line in file:
+#         #CAN ADD TO TWO SEPARATE VARIABLES WITH USE OF COMMA
+#         name, house = line.rstrip().split(",")
+#         # print(f"{name[0]} is in {house[1]}")
+#         print(f"{name} is in {house}")
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        # students.append(f"{name} is in {house}")
+# #SORT BY NAME
+# for student in sorted(students):
+#     print(student)
+        # student = {}
+        # student["name"] = name
+        # student["house"] = house
+        # students.append(student)
+        #MORE COMPACT
+        student = {"name": name, "house": house}
+        students.append(student)
+# SINGLE QUOTES NEEDED INSIDE
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
