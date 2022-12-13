bb01b02f5fc8f7a4eeac451af894ffb1452d6c86
KeeveRW11 <keevewhyte+github@gmail.com>
2022-12-13 11:07:41 -0500

    complete fileIO

--- 7. File IO/students.py
+++ 7. File IO/students.py
@@ -11,26 +11,60 @@
 #         # print(f"{name[0]} is in {house[1]}")
 #         print(f"{name} is in {house}")
 
-students = []
+# students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        # students.append(f"{name} is in {house}")
-# #SORT BY NAME
-# for student in sorted(students):
-#     print(student)
-        # student = {}
-        # student["name"] = name
-        # student["house"] = house
-        # students.append(student)
-        #MORE COMPACT
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         # students.append(f"{name} is in {house}")
+# # #SORT BY NAME
+# # for student in sorted(students):
+# #     print(student)
+#         # student = {}
+#         # student["name"] = name
+#         # student["house"] = house
+#         # students.append(student)
+#         #MORE COMPACT
+#         student = {"name": name, "house": house}
+#         students.append(student)
 
-def get_name(student):        
-    return student["name"]
-# SINGLE QUOTES NEEDED INSIDE
-# passing functions as arguments
-for student in sorted(students, key=get_name):
-    print(f"{student['name']} is in {student['house']}")
+# # def get_name(student):        
+# #     return student["name"]
+# # # SINGLE QUOTES NEEDED INSIDE
+# # # passing functions as arguments
+# # for student in sorted(students, key=get_name):
+# #     print(f"{student['name']} is in {student['house']}")
+# # def get_name(student):        
+# #     return student["name"]
+# # SINGLE QUOTES NEEDED INSIDE
+# # passing LAMBDA functions as arguments
+# for student in sorted(students, key=lambda student:student["name"]):
+#     print(f"{student['name']} is in {student['house']}")
+# import csv
+
+# students = []
+# #CSV Reader
+# # with open("students.csv") as file:
+# #         reader = csv.reader(file)
+# #         for row in reader:
+# #             students.append({"name": row[0], "home": row[1]})
+# #DictReader
+# with open("students.csv") as file:
+#         reader = csv.DictReader(file)
+#         for row in reader:
+#             students.append({"name": row["name"], "home": row["home"]})
+
+# for student in sorted(students, key=lambda student:student["name"]):
+#     print(f"{student['name']} is from {student['home']}")
+import csv 
+
+name = input ("What's your name? ")
+home = input ("Where's your home? ")
+
+# with open("students.csv", "a") as file:
+#     writer = csv.writer(file)
+#     writer.writerow([name, home])
+
+with open("students.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
