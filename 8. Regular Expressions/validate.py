f8334f93542a8f526318a573fb97c49be0c16d32
KeeveRW11 <keevewhyte+github@gmail.com>
2022-12-22 18:09:22 -0500

    regex patterns

--- 8. Regular Expressions/validate.py
+++ 8. Regular Expressions/validate.py
@@ -1,12 +1,23 @@
+#WITHOUT REGEX
+# email = input("Whats your email? ").strip()
+
+# # if "@" in email and ".":
+# #     print("Valid")
+# # else:
+# #     print("Invalid")
+# username, domain = email.split("@")
+
+# if (username) and ("." in domain):
+#     print("valid")
+# else:
+#     print("Invalid")    
+#WITH REGEX    
+
+import re
+
 email = input("Whats your email? ").strip()
 
-# if "@" in email and ".":
-#     print("Valid")
-# else:
-#     print("Invalid")
-username, domain = email.split("@")
-
-if (username) and ("." in domain):
-    print("valid")
+if re.search(r".+@.+\.edu", email):
+    print("Valid")
 else:
     print("Invalid")    
