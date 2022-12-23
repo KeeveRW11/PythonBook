ca6b3310e9f0f4fed51ac83934826aa9afc639f3
KeeveRW11 <keevewhyte+github@gmail.com>
2022-12-22 20:50:16 -0500

    groups

--- 8. Regular Expressions/validate.py
+++ 8. Regular Expressions/validate.py
@@ -17,7 +17,7 @@ import re
 
 email = input("Whats your email? ").strip()
 
-if re.search(r".+@.+\.edu", email):
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")    
